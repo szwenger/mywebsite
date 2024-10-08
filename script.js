d65e94d0a311cd7a7aef2681cf093b690d683334
szwenger <149849296+szwenger@users.noreply.github.com>
2024-10-08 23:35:44 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-function showTime() {
-  document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
+document.addEventListener('DOMContentLoaded', function() {
+  function showTime() {
+    document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+  }
   showTime();
-}, 1000);
+  setInterval(function () {
+    showTime();
+  }, 1000);
 
-document.getElementById('homeButton').addEventListener('click', function() {
-  document.getElementById('content').innerHTML = `
-    <h2>7th October 2024</h2>
-    <p>This is my personal blog, currently WIP. I will soon be dumping all of my personal thoughts and stuff here! So bookmark, if you want to follow along my journey.</p>
-    <h2>8th October 2024</h2>
-    <p>As you can probably tell, this website is pretty bare right now. This will change over time, because as I get more and more used to html5 coding, this website will get more and more advanced.</p>
-  `;
-});
+  document.getElementById('homeButton').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = `
+      <h2>7th October 2024</h2>
+      <p>This is my personal blog, currently WIP. I will soon be dumping all of my personal thoughts and stuff here! So bookmark, if you want to follow along my journey.</p>
+      <h2>8th October 2024</h2>
+      <p>As you can probably tell, this website is pretty bare right now. This will change over time, because as I get more and more used to html5 coding, this website will get more and more advanced.</p>
+    `;
+  });
 
-document.getElementById('subMenuButton').addEventListener('click', function() {
-  document.getElementById('content').innerHTML = `
-    <h2>Youtube</h2>
-    <p></p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/Fy9UYGzPVG8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  `;
+  document.getElementById('subMenuButton').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = `
+      <h2>Youtube</h2>
+      <p></p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Fy9UYGzPVG8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `;
+  });
 });
